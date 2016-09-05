9ec1699305032923aca9cf1b27f6746d9c051b3d
Alexey Pelykh <alexey.pelykh@gmail.com>
2016-09-05 22:51:50 +0300

    Update

---
+++ AndroidBridge/library/src/main/java/com/playseeds/unity3d/androidbridge/InAppMessageStatsListenerBridge.java
@@ -0,0 +1,16 @@
+package com.playseeds.unity3d.androidbridge;
+
+import com.playseeds.android.sdk.IInAppMessageStatsListener;
+import com.unity3d.player.UnityPlayer;
+
+public class InAppMessageStatsListenerBridge implements IInAppMessageStatsListener {
+    private String unityObjectName;
+
+    private InAppMessageStatsListenerBridge(String unityObjectName) {
+        this.unityObjectName = unityObjectName;
+    }
+
+    public void onInAppMessageStats(String key, int shownCount) {
+        UnityPlayer.UnitySendMessage(unityObjectName, "onInAppMessageStats", key);
+    }
+}
